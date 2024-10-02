c8698c61e8b807978a8472d0257b621817e608fd
kobycut <kobycut@gmail.com>
2024-10-02 10:30:54 -0600

    updated

--- Project2/convex_hull.py
+++ Project2/convex_hull.py
@@ -1,6 +1,8 @@
 # Uncomment this line to import some functions that can help
 # you debug your algorithm
-# from plotting import draw_line, draw_hull, circle_point
+from plotting import draw_line, draw_hull, circle_point
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -15,6 +17,9 @@ class LinkedList:
     def insertAtBeginning(self, new_data):
         new_node = Node(new_data)
         new_node.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new_node
         self.head = new_node
 
 
@@ -22,15 +27,86 @@ def x_coord(point):
     return point[0]
 
 
-def find_upper_tangent(L, R):
+def get_slope(x1, x2, y1, y2):
+    return (y2 - y1) / (x2 - x1)
+
+def find_lower_tangent(l, r):
     l_hull = LinkedList()
     r_hull = LinkedList()
+    l = sorted(l, key=x_coord)
+    r = sorted(r, key=x_coord, reverse=True)
+    for i in l:
+        l_hull.insertAtBeginning(i)
+    for j in r:
+        r_hull.insertAtBeginning(j)
     p = l_hull.head
     q = r_hull.head
-
+    temp = (p, q)
+    done = False
+    while not done:
+        done = True
+        while True:
+            temp_slope = get_slope(temp[0][0], temp[1][0], temp[0][1], temp[1][1])
+            l_slope = get_slope(p[0], l_hull.head.data[0], p[1], l_hull.head.data[1])
+            if temp_slope > l_slope:
+                r = p.prev
+                temp = (r,q)
+                p = r
+                done = False
+            else:
+                break
+        while True:
+            temp_slope = get_slope(temp[0][0], temp[1][0], temp[0][1], temp[1][1])
+            r_slope = get_slope(q[0], r_hull.head.data[0], q[1], r_hull.head.data[1])
+            if temp_slope > r_slope:
+                r = q.prev
+                temp = (p, r)
+                q = r
+                done = False
+            else:
+                break
+    return temp
+def find_upper_tangent(l, r):
+    l_hull = LinkedList()
+    r_hull = LinkedList()
+    l = sorted(l, key=x_coord)
+    r = sorted(r, key=x_coord, reverse=True)
+    for i in l:
+        l_hull.insertAtBeginning(i)
+    for j in r:
+        r_hull.insertAtBeginning(j)
+    p = l_hull.head
+    q = r_hull.head
+    temp = (p, q)
+    done = False
+    while not done:
+        done = True
+        while True:
+            temp_slope = get_slope(temp[0][0], temp[1][0], temp[0][1], temp[1][1])
+            l_slope =
+            if temp_slope > l_slope:
+                r = p.next
+                temp = (r,q)
+                p = r
+                done = False
+            else:
+                break
+        while True:
+            temp_slope = get_slope(temp[0][0], temp[1][0], temp[0][1], temp[1][1])
+            r_slope =
+            if temp_slope > r_slope:
+                r = q.next
+                temp = (p, r)
+                q = r
+                done = False
+            else:
+                break
+    return temp
 
 def merge(l, r):
-    pass
+    upper_tangent = find_upper_tangent(l, r)
+    lower_tangent = find_lower_tangent(l, r)
+    return upper_tangent, lower_tangent
 
 
 def recursive_helper(points):
@@ -51,4 +127,5 @@ def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]
     return [H]
 
 
-points = []
+points = [(1, 324), (2, 363), (3, 211), (4, 54), (5, 93)]
+print(compute_hull(points))
