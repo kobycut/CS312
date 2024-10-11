c54a6530fa15d7fbc3a7be4e15b78302d4169a9e
kobycut <kobycut@gmail.com>
2024-10-10 20:10:38 -0600

    heap PQ

--- Project3/network_routing.py
+++ Project3/network_routing.py
@@ -1,3 +1,7 @@
+from priority_queues import PriorityQueueArray, PriorityQueueHeap
+import heapq
+
+
 def find_shortest_path_with_heap(
         graph: dict[int, dict[int, float]],
         source: int,
@@ -11,6 +15,36 @@ def find_shortest_path_with_heap(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    dist = {}
+    prev = {}
+    V = list(graph.keys())
+    for node in V:
+        dist[node] = float('inf')
+        prev[node] = None
+
+    dist[source] = 0
+    H = PriorityQueueHeap(V, dist)
+    while H.check_empty() != 0:
+        U = H.pop_min()
+        E = graph[U]
+        for connector_node in E.keys():
+
+            if dist[connector_node] > dist[U] + E[connector_node]:
+                dist[connector_node] = dist[U] + E[connector_node]
+                prev[connector_node] = U
+                H.decrease_key(connector_node, dist[connector_node])
+    previous_node = target
+    path_lst = []
+    while previous_node != source:
+        path_lst.append(previous_node)
+        previous_node = prev[previous_node]
+
+    path_lst.append(source)
+    path_lst.reverse()
+
+    cost_of_path = dist[target]
+
+    return path_lst, cost_of_path
 
 
 def find_shortest_path_with_array(
@@ -26,3 +60,33 @@ def find_shortest_path_with_array(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    dist = {}
+    prev = {}
+    V = list(graph.keys())
+    for node in V:
+        dist[node] = float('inf')
+        prev[node] = None
+
+    dist[source] = 0
+    H = PriorityQueueArray(V, dist)
+    while H.check_empty() != 0:
+        U = H.pop_min()
+        E = graph[U]
+        for connector_node in E.keys():
+
+            if dist[connector_node] > dist[U] + E[connector_node]:
+                dist[connector_node] = dist[U] + E[connector_node]
+                prev[connector_node] = U
+                H.decrease_key(connector_node, dist[connector_node])
+    previous_node = target
+    path_lst = []
+    while previous_node != source:
+        path_lst.append(previous_node)
+        previous_node = prev[previous_node]
+
+    path_lst.append(source)
+    path_lst.reverse()
+
+    cost_of_path = dist[target]
+
+    return path_lst, cost_of_path
--- Project3/priority_queues.py
+++ Project3/priority_queues.py
@@ -1,1 +1,79 @@
-# :)
+class PriorityQueueArray:
+
+    def __init__(self, v, dist):
+        self.PQ = {}
+        self.nodes = v
+        self.dist = dist
+        self.make_queue(self.nodes, self.dist)
+
+    def decrease_key(self, v, dist):
+        self.PQ[v] = dist
+
+    def pop_min(self):
+        min_node = None
+        least = float('inf')
+        for i in self.PQ.keys():
+            dist = self.PQ[i]
+            if dist < least:
+                least = dist
+                min_node = i
+        self.PQ.pop(min_node)
+        return min_node
+
+    def make_queue(self, v, dist):
+        for node in v:
+            self.PQ[node] = dist[node]
+
+    def check_empty(self):
+        return len(self.PQ)
+
+
+class PriorityQueueHeap:
+    def __init__(self, V, dist):
+        self.index_dict = {}
+        self.heap = []
+        self.dist_dict = dist
+        self.make_heap(V)
+
+    def push(self, item):
+        self.heap.append(item)
+        self.index_dict[item] = len(self.heap)
+        while self.dist_dict[item] < self.dist_dict[self.heap[(self.index_dict[item] // 2) - 1]]:
+            self.swap(self.index_dict[item] - 1, (self.index_dict[item] // 2) - 1)
+
+    def check_empty(self):
+        return len(self.heap)
+
+    def swap(self, child_index, parent_index):
+        child_val = self.heap[child_index]
+        parent_val = self.heap[parent_index]
+        self.heap[child_index] = parent_val
+        self.heap[parent_index] = child_val
+        self.index_dict[child_val] = parent_index + 1
+        self.index_dict[parent_val] = child_index + 1
+
+    def pop_min(self):
+        self.swap(0, len(self.heap) - 1)
+        least = self.heap.pop()
+        self.index_dict.pop(least)
+        top = self.heap[0]
+        child1 = self.heap[(self.index_dict[top] * 2) - 1]
+        # check to see if there is a second child.
+        child2 = self.heap[(self.index_dict[top] * 2)]
+        while self.dist_dict[top] > self.dist_dict[child1] or self.dist_dict[top] > self.dist_dict[child2]:
+            lesser_child = child1
+            if self.dist_dict[child2] < self.dist_dict[child1]:
+                lesser_child = child2
+            self.swap(self.index_dict[lesser_child] - 1, self.index_dict[top] - 1)
+            child1 = self.heap[(self.index_dict[top] * 2) - 1]
+            child2 = self.heap[(self.index_dict[top] * 2)]
+        return least
+
+    def decrease_key(self, v, dist):
+        self.dist_dict[v] = dist
+        while self.dist_dict[v] < self.dist_dict[self.heap[(self.index_dict[v] // 2) - 1]]:
+            self.swap(self.index_dict[v] - 1, (self.index_dict[v] // 2) - 1)
+
+    def make_heap(self, V):
+        for i in V:
+            self.push(i)
