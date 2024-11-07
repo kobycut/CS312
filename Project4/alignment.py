288bfae036d80c9958d92bbb4f7cda1191df99a2
kobycut <kobycut@gmail.com>
2024-11-06 20:10:14 -0700

    finished

--- Project4/alignment.py
+++ Project4/alignment.py
@@ -7,19 +7,7 @@ def align(
         banded_width=-1,
         gap='-'
 ) -> tuple[float, str | None, str | None]:
-    """
-        Align seq1 against seq2 using Needleman-Wunsch
-        Put seq1 on left (j) and seq2 on top (i)
-        => matrix[i][j]
-        :param seq1: the first sequence to align; should be on the "left" of the matrix
-        :param seq2: the second sequence to align; should be on the "top" of the matrix
-        :param match_award: how many points to award a match
-        :param indel_penalty: how many points to award a gap in either sequence
-        :param sub_penalty: how many points to award a substitution
-        :param banded_width: banded_width * 2 + 1 is the width of the banded alignment; -1 indicates full alignment
-        :param gap: the character to use to represent gaps in the alignment strings
-        :return: alignment cost, alignment 1, alignment 2
-    """
+
     banded = False
     if banded_width != -1:
         banded = True
@@ -57,7 +45,6 @@ def align(
                     up = float('inf')
 
                 matrix_dict[i, j] = min(diag, up, left)
-
         optimal_cost = matrix_dict[len(seq1), len(seq2)]
 
     else:
@@ -73,7 +60,6 @@ def align(
                 up = matrix_dict[i, j - 1] + indel_penalty
 
                 matrix_dict[i, j] = min(diag, up, left)
-
         optimal_cost = matrix_dict[len(seq1), len(seq2)]
 
     # back track
